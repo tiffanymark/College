f5c11f61a0a9926768cb31369827149a5ccdbe04
Tiffany Mark <tiffmark@hotmail.com>
2016-06-27 16:36:56 -0300

    Initial commit

---
+++ src/test/java/com/mark/CollegeApplicationTests.java
@@ -0,0 +1,18 @@
+package com.mark;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = CollegeApplication.class)
+@WebAppConfiguration
+public class CollegeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

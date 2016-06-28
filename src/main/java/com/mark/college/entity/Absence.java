cdcdf02d1214ef8c9fa4087c1ba8d1abee8fbceb
Tiffany Mark <tiffmark@hotmail.com>
2016-06-28 17:12:31 -0300

    criação da entidade Absence

---
+++ src/main/java/com/mark/college/entity/Absence.java
@@ -0,0 +1,61 @@
+package com.mark.college.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created by Tiffany Mark on 28/06/2016.
+ */
+
+@Entity
+@Table(name = "absence")
+public class Absence implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(name = "subject")
+    private String subject;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id")
+    private Student student;
+
+    @Column(name = "quantity")
+    private int quantity;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+}

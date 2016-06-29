9fe4543205dbc5ffbb0b4bf4713aba921c0fac36
Tiffany Mark <tiffmark@hotmail.com>
2016-06-29 18:38:45 -0300

    alteração no strategy para GenerationType.IDENTITY

--- src/main/java/com/mark/college/entity/Absence.java
+++ src/main/java/com/mark/college/entity/Absence.java
@@ -14,7 +14,7 @@ public class Absence implements Serializable {
     private static final long serialVersionUID = 1L;
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "subject")

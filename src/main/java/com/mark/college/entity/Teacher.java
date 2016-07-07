0adf799336a0e0f0f121b852e473cc6312491d50
Tiffany Mark <tiffmark@hotmail.com>
2016-07-06 23:28:34 -0300

    criação da entidade Teacher

---
+++ src/main/java/com/mark/college/entity/Teacher.java
@@ -0,0 +1,50 @@
+package com.mark.college.entity;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+/**
+ * Created by Tiffany Mark on 06/07/2016.
+ */
+
+@Entity
+@Table(name = "teacher")
+public class Teacher implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(mappedBy = "teacher", cascade = CascadeType.ALL)
+    private Set<Subject> subjects;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Subject> getSubjects() {
+        return subjects;
+    }
+
+    public void setSubjects(Set<Subject> subjects) {
+        this.subjects = subjects;
+    }
+}
